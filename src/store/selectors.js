954be2aa1eb344cdacc3cc0fa85582dc5af89cdd
Andrii Veremii <dashuk10@gmail.com>
2024-03-09 17:01:54 +0100

    add

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -17,27 +17,3 @@ export const getFilteredContacts = createSelector(
 
 
 
-// export const getFilteredContacts = store => {
-//   const { filter, contacts } = store;
-
-//   if (!filter.filter) {
-//     return contacts.contacts;
-//   }
-
-//   const normalizedFilter = filter.filter.toLowerCase();
-//   const visibleContacts = contacts.contacts.filter(contact =>
-//     contact.name.toLowerCase().trim().includes(normalizedFilter)
-//   );
-
-//   if (normalizedFilter && !visibleContacts.length) {
-//     alert('No contacts matching your request');
-//     Report.info(
-//       'Sorry',
-//       'No contacts matching your request',
-//       'Ok'
-//     );
-//   }
-
-//   return visibleContacts;
-// };
-
