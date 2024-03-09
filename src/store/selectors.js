698158b77d4337e08f1c62d61260947402e9b21f
Andrii Veremii <dashuk10@gmail.com>
2024-03-09 16:54:15 +0100

    add

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,28 +1,43 @@
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import { createSelector } from '@reduxjs/toolkit';
 
 export const getContacts = store => store.contacts;
 export const getFilter = store => store.filter;
-export const getFilteredContacts = store => {
-  const { filter, contacts } = store;
 
-  if (!filter.filter) {
-    return contacts.contacts;
-  }
+export const getFilteredContacts = createSelector(
+  [getContacts, getFilter],
 
-  const normalizedFilter = filter.filter.toLowerCase();
-  const visibleContacts = contacts.contacts.filter(contact =>
-    contact.name.toLowerCase().trim().includes(normalizedFilter)
-  );
+  (contacts, filter) => {
+    const normalizedFilter = filter.filter.toLowerCase();
 
-  if (normalizedFilter && !visibleContacts.length) {
-    alert('No contacts matching your request');
-    Report.info(
-      'Sorry',
-      'No contacts matching your request',
-      'Ok'
+    return contacts.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   }
+);
 
-  return visibleContacts;
-};
+
+
+// export const getFilteredContacts = store => {
+//   const { filter, contacts } = store;
+
+//   if (!filter.filter) {
+//     return contacts.contacts;
+//   }
+
+//   const normalizedFilter = filter.filter.toLowerCase();
+//   const visibleContacts = contacts.contacts.filter(contact =>
+//     contact.name.toLowerCase().trim().includes(normalizedFilter)
+//   );
+
+//   if (normalizedFilter && !visibleContacts.length) {
+//     alert('No contacts matching your request');
+//     Report.info(
+//       'Sorry',
+//       'No contacts matching your request',
+//       'Ok'
+//     );
+//   }
+
+//   return visibleContacts;
+// };
 
